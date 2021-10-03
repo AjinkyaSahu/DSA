77ec99b87d13836bf2b42c99b55371ffa84a75b5
viren-vii <virubhosale112@gmail.com>
2021-10-03 10:27:28 +0530

    added template and stack implementation

---
+++ In Cpp/template.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define fi first
+#define se second
+#define PI 3.1415926535897932384626433832795
+#define all(a) a.begin(),a.end()
+#define deb(a) cout<<#a<<"->"<<a<<endl
+#define nl endl
+#define debv(v) for(auto x : v)cout<<x<<" "; cout<<endl;
+#define debv2(v) for(auto x:mp)cout<<x.fi<<" "<<x.se<<"\n";
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define MOD 1000000007
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+#define fill(a, n)              \
+    for (int i = 0; i < n; i++) \
+    cin >> a[i]
+    
+/*
+
+*/
+    
+void solve(){
+ 	
+}
+int main() {
+	// freopen("input.txt","r",stdin);
+	// freopen("output.txt","w",stdout);
+    fast;
+    int t=1;
+    // cin>>t;
+    while(t--)
+        solve();
+    return 0;
+}
