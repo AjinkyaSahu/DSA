d39d4ef7c854daa7d52773778d1951367d0acfb9
Ruhika <65492460+Ruhika1417@users.noreply.github.com>
2021-10-10 20:55:56 +0530

    reverse of any digit no in cpp

--- In Cpp/reverse_no.cpp
+++ In Cpp/reverse_no.cpp
@@ -6,11 +6,11 @@ int main(){
     cin>>n;
     int reverse=0;
 
-    //was using i and j loop with for.wrong
+    
      while(n>0){
-         int lastdigit=n%10;       //n=19;last=9;      //step2:1%10=1
+         int lastdigit=n%10;           //n=19;last=9;      //step2:1%10=1
          reverse=reverse*10+lastdigit;//r=0*10+9=9      //r=9*10+1=91
-         n=n/10;                   //n=1
+         n=n/10;                        //n=1
         
 
      }
