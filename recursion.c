80d0cbe1e16780ca8009b51d72585403ed998770
arpitc146 <84246970+arpitc146@users.noreply.github.com>
2021-10-21 22:46:09 +0530

    Create recursion.c

---
+++ recursion.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+ 
+ int factorial(int number)
+ {
+   if (number == 1 || number == 0)
+
+ { return 1;
+ }
+
+ else {
+     return (number * factorial (number -1));
+
+ }
+}
+ int main()
+ {
+   int num;
+   printf("enter the number you whant the factorial of \n");
+
+   scanf("%d", &num);
+
+   printf("the factrial of %d is %d", num, factorial(num));
+
+
+    return 0;
+
+ }
